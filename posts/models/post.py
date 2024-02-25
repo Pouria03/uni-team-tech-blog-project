b9a850a2c45e27f2b2b9e6c7185c13932d31f513
Pouria03 <pouriashaigani@gmail.com>
2024-02-25 14:54:07 +0330

    the column "writer" added to model Post

--- posts/models/post.py
+++ posts/models/post.py
@@ -1,6 +1,8 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -10,6 +12,7 @@ class Post(models.Model):
     is_active = models.BooleanField(db_default=True)
     categories = models.ForeignKey(
         'Category', on_delete=models.SET_NULL, blank=True, null=True)
-    
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
     def __str__(self):
         return self.title
