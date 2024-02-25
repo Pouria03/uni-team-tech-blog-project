f9792f46d0b3dfe384c8dd7804dd7f2161506331
Pouria03 <pouriashaigani@gmail.com>
2024-02-25 15:57:39 +0330

    the attribute "related_name" of field "parent" in model "Category" removed to use the defualt name for quereis

--- posts/models/category.py
+++ posts/models/category.py
@@ -4,8 +4,7 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=30)
     short_description = models.CharField(max_length=90, null=True, blank=True)
-    parent = models.ForeignKey('self', null=True, blank=True,
-        related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
