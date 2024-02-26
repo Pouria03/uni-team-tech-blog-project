3e43c4f82ddadb017ffd9048489b1becbe9e1f73
Pouria03 <pouriashaigani@gmail.com>
2024-02-26 15:48:31 +0330

    'PUT' and 'DELETE' methods of the model Category added

--- posts/api/v1/urls.py
+++ posts/api/v1/urls.py
@@ -3,4 +3,5 @@ from .views import CategoriesView, CategoryDetailView
 
 urlpatterns = [
     path('v1/categories/', CategoriesView.as_view(), name='categories'),
+    path('v1/categories/<int:id>/', CategoryDetailView.as_view(), name='category-detail'),
 ]
--- posts/api/v1/views.py
+++ posts/api/v1/views.py
@@ -1,12 +1,14 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from posts.models import Category
 from posts.api.v1.serializers import CategorySerializer
 from posts.api.v1.permissions import StaffOrSuperuserPermission
 
 
 class CategoriesView(APIView):
+    serializer_class = CategorySerializer
     permission_classes = [StaffOrSuperuserPermission]
     def post(self, request):
         """Create a category (category or sub category)"""
@@ -18,8 +20,35 @@ class CategoriesView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
-        """Get all Categories"""
+        """Get all Categories with their subcategories"""
         categories = Category.objects.prefetch_related('parent').filter(parent=None)
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class CategoryDetailView(APIView):
+    serializer_class = CategorySerializer
+    permission_classes = [StaffOrSuperuserPermission]
+    def setup(self, request, *args, **kwargs):
+        """This method gets the exact object related to ID that is entered through enpoint"""
+        self.this_category = get_object_or_404(Category, id=kwargs['id'])
+        return super().setup(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        """This method updates attributes of a category or sub category"""
+        serializer = CategorySerializer(data=request.data, instance=self.this_category, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        """This method deletes a category or a sub category"""
+        try:
+            category_title = self.this_category.title
+            self.this_category.delete()
+            return Response({"result":"{} deleted successfully".format(category_title)}, status=status.HTTP_200_OK)
+        except Category.DoesNotExist:
+            return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({"result":"Something went wrong. Try again later"}, status=status.HTTP_400_BAD_REQUEST)
