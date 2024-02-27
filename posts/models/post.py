d0b9a647b544e059e3251e434ed8a9cef0e5b512
Pouria03 <pouriashaigani@gmail.com>
2024-02-27 12:37:57 +0330

    default queryset method of the model Post modified in a way that only returns posts with attribute 'is_active==True'

--- posts/models/post.py
+++ posts/models/post.py
@@ -4,6 +4,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class PostManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active='True')
+
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = RichTextField()
@@ -13,6 +21,8 @@ class Post(models.Model):
     category = models.ForeignKey(
         'Category', on_delete=models.SET_NULL, blank=True, null=True)
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    objects = PostManager()
 
     def __str__(self):
         return self.title
