ddbae00c2ee0661a176654d5d272478f86c79228
Pouria03 <pouriashaigani@gmail.com>
2024-02-27 15:09:01 +0330

    refactoring and clean code in posts/api/v1/views.py

--- posts/api/v1/views.py
+++ posts/api/v1/views.py
@@ -33,13 +33,16 @@ class CategoryDetailView(APIView):
     serializer_class = CategorySerializer
     permission_classes = [StaffOrSuperuserPermission]
     def setup(self, request, *args, **kwargs):
-        """This method gets the exact object related to ID that is entered through enpoint"""
+        """This method gets the exact object,
+          related to ID that is entered through enpoint
+        """
         self.this_category = get_object_or_404(Category, id=kwargs['id'])
         return super().setup(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
         """This method updates attributes of a category or sub category"""
-        serializer = CategorySerializer(data=request.data, instance=self.this_category, partial=True)
+        serializer = CategorySerializer(data=request.data,
+                                         instance=self.this_category, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -50,11 +53,13 @@ class CategoryDetailView(APIView):
         try:
             category_title = self.this_category.title
             self.this_category.delete()
-            return Response({"result":"{} deleted successfully".format(category_title)}, status=status.HTTP_200_OK)
+            return Response({"result":"{} deleted successfully".format(category_title)},
+                             status=status.HTTP_200_OK)
         except Category.DoesNotExist:
             return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
         except:
-            return Response({"result":"Something went wrong. Try again later"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"result":"Something went wrong. Try again later"},
+                             status=status.HTTP_400_BAD_REQUEST)
         
 
 class PostsView(generics.ListCreateAPIView):
@@ -65,7 +70,9 @@ class PostsView(generics.ListCreateAPIView):
     permission_classes = [StaffOrSuperuserPermission]
     pagination_class = PostPagination
     queryset = Post.objects.all()
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend,
+                        filters.SearchFilter,
+                         filters.OrderingFilter]
     filterset_fields = ['category']
     search_fields = ['title', 'content']
     ordering = ['-date_created']     
@@ -75,27 +82,42 @@ class PostDetailView(APIView):
     serializer_class = PostSerializer
     permission_classes = [StaffOrSuperuserPermission]
     def setup(self, request, *args, **kwargs):
+        """This method gets the post object that
+            by the input ID
+        """
         self.this_post = get_object_or_404(Post, id=kwargs['id'])
         return super().setup(request, *args, **kwargs)
     
     def get(self, request, *args, **kwargs):
+        """This method retrieves the post object that
+            specified in setup method by getteign its ID
+        """
         serializer = PostSerializer(self.this_post)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data, instance=self.this_post, partial=True)
+        """This method updates the post object that
+            specified in setup method by getteign its ID
+        """
+        serializer = PostSerializer(data=request.data,
+                                     instance=self.this_post, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, *args, **kwargs):
+        """This method deletes the post object that
+            specified in setup method by getteign its ID
+        """
         try:
             post_title = self.this_post.title
             self.this_post.delete()
-            return Response({"result":"{} deleted successfully".format(post_title)}, status=status.HTTP_200_OK)
+            return Response({"result":"{} deleted successfully".format(post_title)},
+                             status=status.HTTP_200_OK)
         except Category.DoesNotExist:
             return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
         except:
-            return Response({"result":"Something went wrong. Try again later"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"result":"Something went wrong. Try again later"},
+                             status=status.HTTP_400_BAD_REQUEST)
         
