dab6b0de4091b4df3bcbaa1d872ef4f637089321
Pouria03 <pouriashaigani@gmail.com>
2024-02-29 17:56:26 +0330

    POST, PUT, DELETE operations for models Post and Category commented

--- posts/api/v1/urls.py
+++ posts/api/v1/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from .views import CategoriesView, CategoryDetailView, PostDetailView , PostsView               
 
 urlpatterns = [
-    # Category enpoints
     path('v1/categories/', CategoriesView.as_view(), name='categories'),
-    path('v1/categories/<int:id>/', CategoryDetailView.as_view(), name='category-detail'),
-    # Post endpoints
+    # path('v1/categories/<int:id>/', CategoryDetailView.as_view(), name='category-detail'),
     path('v1/posts/', PostsView.as_view(), name='posts'),
     path('v1/posts/<int:id>/', PostDetailView.as_view(), name='post-detail')
 ]
--- posts/api/v1/views.py
+++ posts/api/v1/views.py
@@ -12,15 +12,15 @@ from rest_framework import filters
 
 class CategoriesView(APIView):
     serializer_class = CategorySerializer
-    permission_classes = [StaffOrSuperuserPermission]
-    def post(self, request):
-        """Create a category (category or sub category)"""
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def post(self, request):
+    #     """Create a category (category or sub category)"""
+    #     serializer = CategorySerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     else:
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         """Get all Categories with their subcategories"""
@@ -29,41 +29,41 @@ class CategoriesView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class CategoryDetailView(APIView):
-    serializer_class = CategorySerializer
-    permission_classes = [StaffOrSuperuserPermission]
-    def setup(self, request, *args, **kwargs):
-        """This method gets the exact object,
-          related to ID that is entered through enpoint
-        """
-        self.this_category = get_object_or_404(Category, id=kwargs['id'])
-        return super().setup(request, *args, **kwargs)
+# class CategoryDetailView(APIView):
+#     serializer_class = CategorySerializer
+#     permission_classes = [StaffOrSuperuserPermission]
+#     def setup(self, request, *args, **kwargs):
+#         """This method gets the exact object,
+#           related to ID that is entered through enpoint
+#         """
+#         self.this_category = get_object_or_404(Category, id=kwargs['id'])
+#         return super().setup(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        """This method updates attributes of a category or sub category"""
-        serializer = CategorySerializer(data=request.data,
-                                         instance=self.this_category, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, *args, **kwargs):
+#         """This method updates attributes of a category or sub category"""
+#         serializer = CategorySerializer(data=request.data,
+#                                          instance=self.this_category, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, *args, **kwargs):
-        """This method deletes a category or a sub category"""
-        try:
-            category_title = self.this_category.title
-            self.this_category.delete()
-            return Response({"result":"{} deleted successfully".format(category_title)},
-                             status=status.HTTP_200_OK)
-        except Category.DoesNotExist:
-            return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
-        except:
-            return Response({"result":"Something went wrong. Try again later"},
-                             status=status.HTTP_400_BAD_REQUEST)
+#     def delete(self, request, *args, **kwargs):
+#         """This method deletes a category or a sub category"""
+#         try:
+#             category_title = self.this_category.title
+#             self.this_category.delete()
+#             return Response({"result":"{} deleted successfully".format(category_title)},
+#                              status=status.HTTP_200_OK)
+#         except Category.DoesNotExist:
+#             return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
+#         except:
+#             return Response({"result":"Something went wrong. Try again later"},
+#                              status=status.HTTP_400_BAD_REQUEST)
         
 
-class PostsView(generics.ListCreateAPIView):
-    """this api view calss handles POST and GET method for model Post.
+class PostsView(generics.ListAPIView):
+    """this api view calss handles GET method for model Post.
         along with pagination, filtering and search. 
      """
     serializer_class = PostSerializer
@@ -80,7 +80,7 @@ class PostsView(generics.ListCreateAPIView):
 
 class PostDetailView(APIView):
     serializer_class = PostSerializer
-    permission_classes = [StaffOrSuperuserPermission]
+    # permission_classes = [StaffOrSuperuserPermission]
     def setup(self, request, *args, **kwargs):
         """This method gets the post object that
             by the input ID
@@ -95,29 +95,29 @@ class PostDetailView(APIView):
         serializer = PostSerializer(self.this_post)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, *args, **kwargs):
-        """This method updates the post object that
-            specified in setup method by getteign its ID
-        """
-        serializer = PostSerializer(data=request.data,
-                                     instance=self.this_post, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request, *args, **kwargs):
+    #     """This method updates the post object that
+    #         specified in setup method by getteign its ID
+    #     """
+    #     serializer = PostSerializer(data=request.data,
+    #                                  instance=self.this_post, partial=True)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, *args, **kwargs):
-        """This method deletes the post object that
-            specified in setup method by getteign its ID
-        """
-        try:
-            post_title = self.this_post.title
-            self.this_post.delete()
-            return Response({"result":"{} deleted successfully".format(post_title)},
-                             status=status.HTTP_200_OK)
-        except Category.DoesNotExist:
-            return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
-        except:
-            return Response({"result":"Something went wrong. Try again later"},
-                             status=status.HTTP_400_BAD_REQUEST)
+    # def delete(self, request, *args, **kwargs):
+    #     """This method deletes the post object that
+    #         specified in setup method by getteign its ID
+    #     """
+    #     try:
+    #         post_title = self.this_post.title
+    #         self.this_post.delete()
+    #         return Response({"result":"{} deleted successfully".format(post_title)},
+    #                          status=status.HTTP_200_OK)
+    #     except Category.DoesNotExist:
+    #         return Response({"result":"Not found"}, status=status.HTTP_404_NOT_FOUND)
+    #     except:
+    #         return Response({"result":"Something went wrong. Try again later"},
+    #                          status=status.HTTP_400_BAD_REQUEST)
         
